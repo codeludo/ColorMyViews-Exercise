ead7eae7ab1bc1e047a75c392636cdbc61bdc96f
Camilo <jcam.qvision@gmail.com>
2022-05-16 16:31:18 -0500

    codelab finished

--- app/src/main/java/com/codeludo/colormyviews/MainActivity.kt
+++ app/src/main/java/com/codeludo/colormyviews/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var boxFiveText: TextView
     private lateinit var rootConstraintLayout: View
 
+    private lateinit var redButton : Button
+    private lateinit var greenButton : Button
+    private lateinit var yellowButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,6 +31,9 @@ class MainActivity : AppCompatActivity() {
         boxFourText = findViewById(R.id.box_four_text)
         boxFiveText = findViewById(R.id.box_five_text)
         rootConstraintLayout = findViewById(R.id.constraint_layout)
+        redButton = findViewById(R.id.red_button)
+        greenButton = findViewById(R.id.green_button)
+        yellowButton = findViewById(R.id.yellow_button)
 
         setListeners()
     }
@@ -39,6 +47,9 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.red_button -> boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> boxFourText.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> boxFiveText.setBackgroundResource(R.color.my_green)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
@@ -47,7 +58,8 @@ class MainActivity : AppCompatActivity() {
         val clickableViews: List<View> =
             listOf(
                 boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout
+                boxFourText, boxFiveText, rootConstraintLayout,
+                redButton, greenButton, yellowButton
             )
 
         for (item in clickableViews) {
